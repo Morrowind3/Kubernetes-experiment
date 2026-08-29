2a81d3f91735d65f110b8f45bc7bf875e0269e40
Morrowind3 <36103509+Morrowind3@users.noreply.github.com>
2026-08-29 21:04:06 +0200

    Delete free-standing note-to-self,  notes were consolidated in  logseq

--- FatigueWorker/fatigue_service.cpp
+++ FatigueWorker/fatigue_service.cpp
@@ -1,16 +1,5 @@
 #include "fatigue_service.h"
 
-/* NOTE: ** Lesson worth documenting for future reference **
-Initially this used a highly simplified rainflow counting algorithm to more closely mimick real fatigue tracking.
-It means fatigue is measured at each reversal about the squared displacement during the prior half-cycle.
-A worker doesn't need state to process a batch like this. We can have many worker pods and each can do their job and stand in for one another.
-However, when designing for parallelism, what's also important is that our input and output is stateless:
-In this case, some state ended up being relevant for the batch: it could be mid-swing, and then relevant data would be hidden somewhere in a previous batch, or lost between batches.
-In fact, this problem is common enough, that the absence of that problem is known as "Embarrassingly parallel".
-Resolving that would require trickery that would distract from the goal of this project, but it shows it's something you'll often need to account for.
-Bottom line: Statelessness of a worker isn't a property of the worker in isolation that doesn't keep state, but describes its relationship to the surrounding system.
-*/
-
 //Wear increases exponentially with movement speed, disregarding distance travelled.
 //This approach measures per two adjacent samples, which is easier to scale horizontally (see above note).
 grpc::Status FatigueServiceImpl::ProcessBatch(grpc::ServerContext *context,
